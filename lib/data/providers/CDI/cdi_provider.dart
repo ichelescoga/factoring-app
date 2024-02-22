f93b98ccd5ef7ed7c80c1b46b9644d45248f63fa
Pablo Zapeta <zapetapablo@gmail.com>
2024-02-21 20:35:50 -0600

    changes

--- lib/data/providers/CDI/cdi_provider.dart
+++ lib/data/providers/CDI/cdi_provider.dart
@@ -6,7 +6,7 @@ class CDIProvider {
   final http = HttpAdapter();
 
   Future<List<dynamic>> fetchDataTable(String entity) async {
-    final endpoint = "orders/v1/getComponentsByEntity";
+    final endpoint = "entity/v1/getComponents";
 
     try {
       final response = await http.getApiWithBody(
--- lib/views/home/pages/dashboard_page.dart
+++ lib/views/home/pages/dashboard_page.dart
@@ -3,7 +3,6 @@ import 'package:developer_company/shared/resources/dimensions.dart';
 import 'package:developer_company/shared/routes/router_paths.dart';
 import 'package:developer_company/shared/utils/permission_level.dart';
 // import 'package:developer_company/shared/utils/responsive.dart';
-import 'package:developer_company/widgets/AuthorizationWrapper.dart';
 import 'package:developer_company/widgets/app_bar_sidebar.dart';
 import 'package:developer_company/widgets/custom_button_widget.dart';
 import 'package:developer_company/widgets/layout.dart';
@@ -68,50 +67,32 @@ class _DashboardPageState extends State<DashboardPage> {
 
   final List<Map<String, String?>> cdi = [
     {
-      "entityId": "1",
-      "label": "Empresas",
-      "listEndpoint": "orders/v1/getCompanies",
-      "editEndpoint": "orders/v1/editCompany",
-      "addEndpoint": "orders/v1/addCompany",
-      "removeEndpoint": "orders/v1/getCompanies",
-      "getByIdEndpoint": "orders/v1/getCompanyById",
-    },
-    {
-      "entityId": "2",
-      "label": "Proyectos",
-      "listEndpoint": "orders/v1/getProjectsByCompany",
-      "editEndpoint": "orders/v1/editProyect",
-      "addEndpoint": "orders/v1/addProyect",
-      "removeEndpoint": "orders/v1/deleteProyect",
-      "getByIdEndpoint": "orders/v1/getProjectById",
-    },
-    {
-      "entityId": "7",
-      "label": "Unidades",
-      "listEndpoint": "orders/v1/getTypes",
+      "entityId": "8",
+      "label": "Cliente",
+      "listEndpoint": "entity/v1/getEntities/8",
       "editEndpoint": null,
-      "addEndpoint": "orders/v1/addType",
+      "addEndpoint": "entity/v1/addType",
       "removeEndpoint": null,
       "getByIdEndpoint": null,
     },
-    // {
-    //   "entityId": "4",
-    //   "label": "Unidades",
-    //   "listEndpoint": "orders/v1/getTypesByProyect/4",
-    //   "editEndpoint": null,
-    //   "addEndpoint": "orders/v1/addType",
-    //   "removeEndpoint": null,
-    //   "getByIdEndpoint": null,
-    // },
-    // {
-    //   "entityId": "5",
-    //   "label": "Unidades",
-    //   "listEndpoint": "orders/v1/getTypesByProyect/5",
-    //   "editEndpoint": null,
-    //   "addEndpoint": "orders/v1/addType",
-    //   "removeEndpoint": null,
-    //   "getByIdEndpoint": null,
-    // },
+    {
+      "entityId": "9",
+      "label": "Expediente",
+      "listEndpoint": "entity/v1/getEntities/9",
+      "editEndpoint": null,
+      "addEndpoint": "entity/v1/addType",
+      "removeEndpoint": null,
+      "getByIdEndpoint": null,
+    },
+    {
+      "entityId": "10",
+      "label": "Representa Legal",
+      "listEndpoint": "entity/v1/getEntities/10",
+      "editEndpoint": null,
+      "addEndpoint": "entity/v1/addType",
+      "removeEndpoint": null,
+      "getByIdEndpoint": null,
+    },
   ];
 
   Future askPermission() async {
@@ -144,145 +125,25 @@ class _DashboardPageState extends State<DashboardPage> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          AuthorizationWrapper(
-            requestAction: PermissionLevel.analystCreditByClient,
-            child: Column(
-              children: [
-                const SizedBox(height: Dimensions.heightSize),
-                CustomButtonWidget(
-                    text: "Aprobaciones de crédito",
-                    onTap: () =>
-                        Get.toNamed(RouterPaths.ANALYST_CREDITS_BY_CLIENT_PAGE),
-                    padding: const EdgeInsets.only(left: 0.0, right: 0.0)),
-              ],
-            ),
-          ),
-          AuthorizationWrapper(
-              requestAction: PermissionLevel.dashboardAddQuoteButton,
-              child: Column(
+          ...cdi
+              .map((e) => Column(
                 children: [
                   spaceButton,
                   CustomButtonWidget(
-                    text: "Creación de solicitud de crédito",
-                    onTap: () => Get.toNamed(RouterPaths.UNIT_QUOTE_PAGE),
-                    padding: defaultPadding,
-                  ),
+                      text: e["label"].toString(),
+                      onTap: () => Get.toNamed(RouterPaths.LIST_CDI_PAGE,
+                              arguments: {
+                                "entityId": e["entityId"],
+                                "label": e["label"],
+                                "listEndpoint": e["listEndpoint"],
+                                "editEndpoint": e["editEndpoint"],
+                                "addEndpoint": e["addEndpoint"],
+                                "removeEndpoint": e["removeEndpoint"],
+                                "getByIdEndpoint": e["getByIdEndpoint"],
+                              }),
+                      padding: defaultPadding),
                 ],
-              )),
-          const SizedBox(height: Dimensions.heightSize),
-          AuthorizationWrapper(
-            requestAction: PermissionLevel.dashboardQueryQuoteButton,
-            child: GestureDetector(
-              child: Container(
-                height: 50.0,
-                width: Get.width,
-                decoration: const BoxDecoration(
-                    color: AppColors.mainColor,
-                    borderRadius:
-                        BorderRadius.all(Radius.circular(Dimensions.radius))),
-                child: const Center(
-                  child: Text(
-                    "Consulta de cotizaciones por unidad",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: Dimensions.largeTextSize,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              onTap: () {
-                Get.toNamed(RouterPaths.QUOTE_UNIT_STATUS_PAGE);
-              },
-            ),
-          ),
-          AuthorizationWrapper(
-            requestAction: PermissionLevel.adviserCreditsApprovedAndReserved,
-            child: Column(
-              children: [
-                spaceButton,
-                CustomButtonWidget(
-                    text: "Créditos Aprobados",
-                    onTap: () => Get.toNamed(
-                        RouterPaths.ADVISER_CREDITS_RESERVED_APPROVED),
-                    padding: defaultPadding),
-              ],
-            ),
-          ),
-          AuthorizationWrapper(
-            requestAction: PermissionLevel.marketingMaintenance,
-            child: Column(
-              children: [
-                spaceButton,
-                CustomButtonWidget(
-                    text: "Material Audio Visual",
-                    onTap: () => Get.toNamed(
-                        RouterPaths.MARKETING_MAINTENANCE_ALBUMS,
-                        arguments: {"isWatchMode": false}),
-                    padding: defaultPadding),
-              ],
-            ),
-          ),
-          AuthorizationWrapper(
-            requestAction: PermissionLevel.discountsByQuote,
-            child: Column(
-              children: [
-                spaceButton,
-                CustomButtonWidget(
-                    text: "Descuentos",
-                    onTap: () => Get.toNamed(
-                        RouterPaths.DISCOUNTS_BY_QUOTE_PAGE,
-                        arguments: {"isWatchMode": false}),
-                    padding: defaultPadding),
-              ],
-            ),
-          ),
-          // AuthorizationWrapper(
-          //   requestAction: PermissionLevel.manageCompany,
-          //   child: Column(
-          //     children: [
-          //       spaceButton,
-          //       CustomButtonWidget(
-          //           text: "Empresas",
-          //           onTap: () => Get.toNamed(RouterPaths.LIST_COMPANIES_PAGE),
-          //           padding: defaultPadding),
-          //     ],
-          //   ),
-          // ),
-          // AuthorizationWrapper(
-          //   requestAction: PermissionLevel.manageCompany,
-          //   child: Column(
-          //     children: [
-          //       spaceButton,
-          //       CustomButtonWidget(
-          //           text: "Proyectos",
-          //           onTap: () =>
-          //               Get.toNamed(RouterPaths.LIST_COMPANY_PROJECTS_PAGE),
-          //           padding: defaultPadding),
-          //     ],
-          //   ),
-          // ),
-          ...cdi
-              .map((e) => AuthorizationWrapper(
-                    requestAction: PermissionLevel.list_cdi,
-                    child: Column(
-                      children: [
-                        spaceButton,
-                        CustomButtonWidget(
-                            text: e["label"].toString(),
-                            onTap: () => Get.toNamed(RouterPaths.LIST_CDI_PAGE,
-                                    arguments: {
-                                      "entityId": e["entityId"],
-                                      "label": e["label"],
-                                      "listEndpoint": e["listEndpoint"],
-                                      "editEndpoint": e["editEndpoint"],
-                                      "addEndpoint": e["addEndpoint"],
-                                      "removeEndpoint": e["removeEndpoint"],
-                                      "getByIdEndpoint": e["getByIdEndpoint"],
-                                    }),
-                            padding: defaultPadding),
-                      ],
-                    ),
-                  ))
+              ))
               .toList(),
         ],
       ),
