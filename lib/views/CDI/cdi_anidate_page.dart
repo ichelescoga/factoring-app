f86d6eee41ec8bcba23d13676737a06d16ecd873
Pablo Zapeta <zapetapablo@gmail.com>
2024-03-11 11:25:03 -0600

    new flow implemented

--- lib/views/CDI/cdi_anidate_page.dart
+++ lib/views/CDI/cdi_anidate_page.dart
@@ -1,5 +1,6 @@
 import 'package:developer_company/shared/resources/dimensions.dart';
 import 'package:developer_company/shared/routes/router_paths.dart';
+import 'package:developer_company/widgets/CDI/card_with_button_list_widget.dart';
 import 'package:developer_company/widgets/app_bar_title.dart';
 import 'package:developer_company/widgets/custom_button_widget.dart';
 import 'package:developer_company/widgets/layout.dart';
@@ -34,57 +35,55 @@ class _CDISecondLevelState extends State<CDISecondLevel> {
         child: Column(
           children: [
             ...mods.map((e) {
-              return e["type"] == "CUSTOM_PATH"
-                  ? Column(
-                      children: [
-                        spaceButton,
-                        CustomButtonWidget(
-                          text: e["label"]!,
-                          onTap: () {
-                            Get.toNamed(e["ROUTE"], arguments: {
-                              "entityId": e["entityId"],
-                              "label": e["label"],
-                              "listEndpoint": e["listEndpoint"]
-                                  .toString()
-                                  .replaceAll(
-                                      "CLIENT_ID", arguments["id"].toString()),
-                              "editEndpoint": e["editEndpoint"],
-                              "addEndpoint": e["addEndpoint"],
-                              "removeEndpoint": e["removeEndpoint"],
-                              "getByIdEndpoint": e["getByIdEndpoint"],
-                              "mods": e["mods"] == null ? [] : e["mods"],
-                              "father": arguments["father"].toString()
-                            });
-                          },
-                          padding: defaultPadding,
-                        ),
-                      ],
-                    )
-                  : Column(
-                      children: [
-                        spaceButton,
-                        CustomButtonWidget(
-                          text: e["label"]!,
-                          onTap: () {
-                            Get.toNamed(RouterPaths.LIST_CDI_PAGE, arguments: {
-                              "entityId": e["entityId"],
-                              "label": e["label"],
-                              "listEndpoint": e["listEndpoint"]
-                                  .toString()
-                                  .replaceAll(
-                                      "CLIENT_ID", arguments["id"].toString()),
-                              "editEndpoint": e["editEndpoint"],
-                              "addEndpoint": e["addEndpoint"],
-                              "removeEndpoint": e["removeEndpoint"],
-                              "getByIdEndpoint": e["getByIdEndpoint"],
-                              "mods": e["mods"] == null ? [] : e["mods"],
-                              "father": arguments["father"].toString()
-                            });
-                          },
-                          padding: defaultPadding,
-                        ),
-                      ],
-                    );
+              final args = {
+                "dataId": e["id"], // in the previous page was defined to id but is dataId
+                "entityId": e["entityId"],
+                "label": e["label"],
+                "listEndpoint": e["listEndpoint"]
+                    .toString()
+                    .replaceAll("CLIENT_ID", arguments["id"].toString()),
+                "editEndpoint": e["editEndpoint"],
+                "addEndpoint": e["addEndpoint"],
+                "removeEndpoint": e["removeEndpoint"],
+                "getByIdEndpoint": e["getByIdEndpoint"],
+                "mods": e["mods"] == null ? [] : e["mods"],
+                "father": arguments["father"].toString()
+              };
+
+              if (e["type"] == "CUSTOM_CARD")
+                return CardWithButtonList(
+                  title: e["label"],
+                  subtitle: e["subTitle"],
+                  buttonList: e["options"],
+                  routeParams: args,
+                );
+
+              if (e["type"] == "CUSTOM_PATH")
+                return Column(
+                  children: [
+                    spaceButton,
+                    CustomButtonWidget(
+                      text: e["label"]!,
+                      onTap: () {
+                        Get.toNamed(e["ROUTE"], arguments: args);
+                      },
+                      padding: defaultPadding,
+                    ),
+                  ],
+                );
+
+              return Column(
+                children: [
+                  spaceButton,
+                  CustomButtonWidget(
+                    text: e["label"]!,
+                    onTap: () {
+                      Get.toNamed(RouterPaths.LIST_CDI_PAGE, arguments: args);
+                    },
+                    padding: defaultPadding,
+                  ),
+                ],
+              );
             }).toList()
           ],
         ));
---
+++ lib/widgets/CDI/card_with_button_list_widget.dart
@@ -0,0 +1,98 @@
+import 'package:developer_company/shared/resources/colors.dart';
+import 'package:developer_company/shared/routes/router_paths.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CardWithButtonList extends StatefulWidget {
+  final String title;
+  final String subtitle;
+  final List<dynamic> buttonList;
+  final Map<String, dynamic> routeParams;
+
+  CardWithButtonList(
+      {Key? key,
+      required this.title,
+      required this.subtitle,
+      required this.buttonList,
+      required this.routeParams})
+      : super(key: key);
+
+  @override
+  _CardWithButtonListState createState() => _CardWithButtonListState();
+}
+
+class _CardWithButtonListState extends State<CardWithButtonList>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+  }
+
+  handleManageData(element) async {
+    Get.toNamed(RouterPaths.MANAGE_CDI_PAGE, arguments: {
+      "dataId": null,
+      "entityId": element["entityId"],
+      "editEndpoint": element["editEndpoint"],
+      "addEndpoint": element["addEndpoint"],
+      "principalLabel": element["label"],
+      "getByIdEndpoint": element["getByIdEndpoint"],
+      "father": widget.routeParams["father"]
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _controller.isDismissed ? _controller.forward() : _controller.reverse();
+      },
+      child: Card(
+        elevation: 4.0,
+        margin: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(widget.title),
+              subtitle: Text(widget.subtitle),
+            ),
+            SizeTransition(
+              sizeFactor: _animation,
+              axisAlignment: -1.0,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                width: double.infinity,
+                child: Column(
+                  children: widget.buttonList
+                      .map((element) => SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                print("values ${element}");
+                                handleManageData(element);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: AppColors.softMainColor,
+                                foregroundColor: Colors.white,
+                              ),
+                              child: Text(element["label"]),
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
