f2c30adc1620e58d333d765522b2ce5c674e1e29
Carlos Zepeda <carlos.ihernandez7@gmail.com>
2024-03-13 15:25:22 -0600

    add constants Body Keys

---
+++ lib/shared/constants/components_body_keys.dart
@@ -0,0 +1,12 @@
+class ComponentBodyKey {
+  static String nombre = "nombre";
+  static String description = "descripcion";
+  static String amountAssignment = "38";
+  static String daysOfCredit = "39";
+  static String interestRate = "40";
+  static String commissionRate = "41";
+  static String dpi = "43";
+  static String phone = "46";
+  static String address = "48";
+  static String isActive = "71";
+}
