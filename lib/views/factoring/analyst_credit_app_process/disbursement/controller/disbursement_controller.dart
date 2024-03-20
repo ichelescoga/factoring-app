749cad9d82453a7fa3ac9dad48e28ed47eabb9f0
Carlos Zepeda <carlos.ihernandez7@gmail.com>
2024-03-20 16:16:22 -0600

    add disbursement options page

---
+++ lib/views/factoring/analyst_credit_app_process/disbursement/controller/disbursement_controller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class FactoringDisbursementController extends GetxController {
+  TextEditingController utilization = TextEditingController();
+  TextEditingController amountInterest = TextEditingController();
+  TextEditingController commissions = TextEditingController();
+  TextEditingController iva = TextEditingController();
+  TextEditingController discounts = TextEditingController();
+  TextEditingController disbursement = TextEditingController();
+}
